0f0597e5a5518afb599ebb35c73652c33c0fffd5
Mateus Brasil <Mateus-code-98>
2022-08-03 09:07:05 -0300

    Preparando revisão de código

--- src/routes/userRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { bodyMiddleware } from "../middlewares/userMiddlewares.js";
-
-const userRouter = Router();
-
-userRouter.post("/sign-up", bodyMiddleware);
-
-export default userRouter;
--- src/utils/token.ts
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const tokenGenerator = (userId: number, email: string) => {
-    const secretKey = process.env.SECRET_KEY;
-    const token = jwt.sign({id: userId, email}, secretKey || "gambiarra :)");
-    return token;
-};
-
-export default tokenGenerator;
